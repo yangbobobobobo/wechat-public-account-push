dc2aba8526815b22eb976cbaa4bd7fbf79a14db3
15738767375 <2645271541@qq.com>
2023-05-06 10:48:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '*生日', name: '☀', year: '1999', date: '07-02',
+          type: '*生日', name: '❤️', year: '1999', date: '07-02',
         },
         //{
           //type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
