abc6fc109f5541ec0a34c04d3d581448ee6d14dd
15738767375 <2645271541@qq.com>
2023-05-06 09:44:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'otV-_6i6KtJaR5PvuR4LVNqcWLAA',
+      id: 'otV-_6m26ZqvwYI5bDwJcyr5nHpQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '36CXDH_EHmNnY5JSGoZ1c1CvY49Pd8qSEFrYs4NY4DI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
