f1d4d5dc586e3772835bd5a672677c665c9b8048
15738767375 <2645271541@qq.com>
2023-05-06 10:45:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otV-_6i6KtJaR5PvuR4LVNqcWLAA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pXLTlWPAMy5XsD4v-fBEc8wHIC2CukqoBlYaGfzxJsY',
+      useTemplateId: 'kheTcGwgcBy8SKADp0NxNr0V2euN4TQqeR9zOvwBWbY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-19',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otV-_6m26ZqvwYI5bDwJcyr5nHpQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pXLTlWPAMy5XsD4v-fBEc8wHIC2CukqoBlYaGfzxJsY',
+      useTemplateId: 'kheTcGwgcBy8SKADp0NxNr0V2euN4TQqeR9zOvwBWbY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-19',
       festivals: [
@@ -63,7 +63,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '*生日', name: '☀', year: '1999', date: '07-02',
+          type: '*生日', name: '☀️', year: '1999', date: '07-02',
         },
         //{
           //type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
